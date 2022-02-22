721086d458fb83bf475086e4aed7f659f310ea27
JesusRuiz02 <zap295@amerike.edu.mx>
2022-02-21 22:48:49 -0600

    Final Version

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,57 +1,59 @@
 using UnityEngine;
+
 public class PlayerMovement : MonoBehaviour
 { 
     [SerializeField] private int _speed = 3;
-    private bool Direction = true;
-    private Rigidbody2D Rb2d = null;
+    private Rigidbody2D _rigidbody2D = null;
     [SerializeField] private float _jumpforce = 5.0f;
-    [SerializeField] private int jumps = 2;
+    [SerializeField] private int _jumps = 2;
     void Start()
     {
-        Rb2d = GetComponent<Rigidbody2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
+    
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            Direction = true;
-            PlayerController(Direction, _speed);
+            PlayerController(true, _speed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Direction = false;
-            PlayerController(Direction, _speed);
+            PlayerController(false, _speed);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump_DoubleJump();
         }
     }
-    void PlayerController( bool dirX, int speed)
+    
+    private void PlayerController( bool dirX, int speed)
     {
         if (dirX)
         {
-            transform.position = transform.position + new Vector3(-(speed)*Time.deltaTime,0,0); 
+            transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0, 0); 
         }
         else
         {
-            transform.position = transform.position +new Vector3(speed*Time.deltaTime,0,0); 
+            transform.position = transform.position +new Vector3(speed * Time.deltaTime, 0, 0); 
         }
     }
-    void Jump_DoubleJump()
+    
+   private void Jump_DoubleJump()
     {
-        if (jumps > 0)
+        if (_jumps > 0)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, _jumpforce), ForceMode2D.Impulse);
-            jumps--;
+            _jumps--;
         }
-        if (jumps == 0) return; 
+        if (_jumps == 0) return; 
     }
+    
     private void OnCollisionEnter2D(Collision2D collider)
     {
-        if (collider.gameObject.tag =="Floor")
+        if (collider.gameObject.tag == "Floor")
         {
-            jumps = 2;
+            _jumps = 2;
         }
     }
 }
