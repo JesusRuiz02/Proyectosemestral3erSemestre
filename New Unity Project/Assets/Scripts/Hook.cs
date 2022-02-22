6b21fa7928a2e3846040a946b537b3b905402212
Pollogamerz <85629754+Pollogamerz@users.noreply.github.com>
2022-02-22 08:56:12 -0600

    Hook corrected

--- New Unity Project/Assets/Scripts/Hook.cs
+++ New Unity Project/Assets/Scripts/Hook.cs
@@ -1,19 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
-    public Camera mainCamera;
-    public LineRenderer _lineRenderer;
-    public DistanceJoint2D _distanceJoint;
-    // Start is called before the first frame update
+    [SerializeField] private Camera mainCamera = default;
+    [SerializeField] private LineRenderer _lineRenderer = default;
+    [SerializeField] private DistanceJoint2D _distanceJoint = default;
+    
     void Start()
     {
         _distanceJoint.enabled = false;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse1))
@@ -25,11 +23,13 @@ public class Hook : MonoBehaviour
             _distanceJoint.enabled = true;
             _lineRenderer.enabled = true;
         }
+
         else if (Input.GetKeyUp(KeyCode.Mouse1))
         {
             _distanceJoint.enabled = false;
             _lineRenderer.enabled = false;
         }
+
         if (_distanceJoint.enabled)
         {
             _lineRenderer.SetPosition(1, transform.position);
