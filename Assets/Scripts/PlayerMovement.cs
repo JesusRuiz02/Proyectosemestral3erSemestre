5743d064268b657a9bee9d4864e3290cf7a729ee
JesusRuiz02 <zap295@amerike.edu.mx>
2022-02-21 18:45:27 -0600

    Jump and double jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] 
     private int _speed = 3; 
     Rigidbody2D rb2d;
+    [SerializeField] 
+    private float _jumpforce = 5.0f;
+    [SerializeField]
+    private int jumps = 2;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +24,52 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = transform.position + new Vector3(-(_speed)*Time.deltaTime,0,0);
+            ControllerPlayerLeft();
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-           transform.position = transform.position + new Vector3(_speed*Time.deltaTime,0,0);
+            ControllerPlayerRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+             Jump_DoubleJump();
         }
 
     }
 
-    void ControllerPlayer()
+    void ControllerPlayerLeft()
     {
-        
+        transform.position = transform.position + new Vector3(-(_speed) * Time.deltaTime, 0, 0);
+    }
+    void ControllerPlayerRight()
+    {
+        transform.position = transform.position + new Vector3(_speed*Time.deltaTime,0,0);
+    }
+
+    void Jump_DoubleJump()
+    {
+        if (jumps > 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, _jumpforce), ForceMode2D.Impulse);
+            jumps--;
+        }
+
+        if (jumps == 0)
+        {
+            return;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag =="Floor")
+        {
+            jumps = 2;
+        }
     }
 }
