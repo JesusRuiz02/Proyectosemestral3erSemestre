212785252e96405130a79ee78e484976495906b9
JesusRuiz02 <zap295@amerike.edu.mx>
2022-02-22 08:22:06 -0600

    AHHH

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
-{ 
+{
+    private const string FLOOR = "Floor";
+    
     [SerializeField] private int _speed = 3;
     private Rigidbody2D _rigidbody2D = null;
     [SerializeField] private float _jumpforce = 5.0f;
     [SerializeField] private int _jumps = 2;
+    [SerializeField] private int _basejumps = 2;
+    
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -23,37 +26,37 @@ public class PlayerMovement : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump_DoubleJump();
+            DoubleJump();
         }
     }
     
-    private void PlayerController( bool dirX, int speed)
+    private void PlayerController( bool _moveRight, int speed)
     {
-        if (dirX)
+        if (_moveRight)
         {
             transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0, 0); 
         }
         else
-        {
-            transform.position = transform.position +new Vector3(speed * Time.deltaTime, 0, 0); 
+        { 
+             transform.position = transform.position +new Vector3(speed * Time.deltaTime, 0, 0); 
         }
     }
     
-   private void Jump_DoubleJump()
+   private void DoubleJump()
+   
     {
         if (_jumps > 0)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, _jumpforce), ForceMode2D.Impulse);
+            _rigidbody2D.AddForce(Vector2.up * _jumpforce, ForceMode2D.Impulse);
             _jumps--;
         }
-        if (_jumps == 0) return; 
     }
     
     private void OnCollisionEnter2D(Collision2D collider)
     {
-        if (collider.gameObject.tag == "Floor")
+        if (collider.gameObject.tag == FLOOR)
         {
-            _jumps = 2;
+            _jumps = _basejumps;
         }
     }
 }
