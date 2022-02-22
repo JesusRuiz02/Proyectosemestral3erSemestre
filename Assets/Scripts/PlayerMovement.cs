f81435fb7152afb76efcc9ed93de2dee4c643aa3
JesusRuiz02 <zap295@amerike.edu.mx>
2022-02-21 22:39:57 -0600

    Final Version

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,38 @@
 using System;
 using UnityEngine;
+
 public class PlayerMovement : MonoBehaviour
 { 
     [SerializeField] private int _speed = 3;
-    private bool Direction = true;
-    private Rigidbody2D Rb2d = null;
+    private Rigidbody2D _rigidbody2D = null;
+    
     void Start()
     {
-        Rb2d = GetComponent<Rigidbody2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
-      if (Input.GetKey(KeyCode.A))
-      {
-        Direction = true;
-        PlayerController(Direction, _speed);
-      }
-      if (Input.GetKey(KeyCode.D))
-      {
-        Direction = false;
-        PlayerController(Direction, _speed);
-      }
+        if (Input.GetKey(KeyCode.A))
+        {
+            PlayerController(true, _speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlayerController(false, _speed);
+        }
     }
+
     void PlayerController( bool dirX, int speed)
     {
         if (dirX)
         {
-            transform.position = transform.position + new Vector3(-(speed)*Time.deltaTime,0,0); 
+            transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0, 0); 
         }
         else
         {
-            transform.position = transform.position +new Vector3(_speed*Time.deltaTime,0,0); 
+            transform.position = transform.position +new Vector3(speed*Time.deltaTime, 0, 0); 
         }
     }
 }
