4409e6ea250b98de5798d19c038420adc874dfe8
JesusRuiz02 <zap295@amerike.edu.mx>
2022-02-21 20:24:30 -0600

    Version 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,37 +1,36 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
-{
-    [SerializeField] 
-    private int _speed = 3; 
-    Rigidbody2D rb2d;
-    // Start is called before the first frame update
+{ 
+    [SerializeField] private int _speed = 3;
+    private bool Direction = true;
+    private Rigidbody2D Rb2d = null;
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        Rb2d = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-       
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = transform.position + new Vector3(-(_speed)*Time.deltaTime,0,0);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-           transform.position = transform.position + new Vector3(_speed*Time.deltaTime,0,0);
-        }
-
+      if (Input.GetKey(KeyCode.A))
+      {
+        Direction = true;
+        PlayerController(Direction, _speed);
+      }
+      if (Input.GetKey(KeyCode.D))
+      {
+        Direction = false;
+        PlayerController(Direction, _speed);
+      }
     }
-
-    void ControllerPlayer()
+    void PlayerController( bool dirX, int speed)
     {
-        
+        if (dirX)
+        {
+            transform.position = transform.position + new Vector3(-(speed)*Time.deltaTime,0,0); 
+        }
+        else
+        {
+            transform.position = transform.position +new Vector3(_speed*Time.deltaTime,0,0); 
+        }
     }
 }
