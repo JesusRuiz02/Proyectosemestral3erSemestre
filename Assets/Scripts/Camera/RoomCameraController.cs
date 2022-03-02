4db6fe89d7c7feb5b3269f153559610af80ec10e
JesusRuiz02 <zap295@amerike.edu.mx>
2022-03-02 10:09:00 -0600

    1

--- Assets/Scripts/Camera/RoomCameraController.cs
+++ Assets/Scripts/Camera/RoomCameraController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ChaningCamera : MonoBehaviour
+public class RoomCameraController : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
     [SerializeField] private Vector3 _positioncamera = new Vector3(17, -0.5f, -10);
@@ -9,11 +9,11 @@ public class ChaningCamera : MonoBehaviour
     {
         if (collider.gameObject.tag == "Player")
         {
-         ChangingCamera(_positioncamera);   
+         ChangeCameraPosition(_positioncamera);   
         }
     }
 
-    private void ChangingCamera(Vector3 position)
+    private void ChangeCameraPosition(Vector3 position)
     {
         _camera.transform.position = position;
     }
