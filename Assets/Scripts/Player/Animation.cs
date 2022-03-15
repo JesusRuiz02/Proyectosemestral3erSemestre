2d6c22c43f3c0c6707aa6264b73f09a879589f3d
Justpeli <83651771+Justpeli@users.noreply.github.com>
2022-03-15 08:10:41 -0600

    Iddle and run

---
+++ Assets/Scripts/Player/Animation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+    
+}
