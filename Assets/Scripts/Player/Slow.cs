2b247f4f2ae04a49575c734b4c527b1d962338e9
Miguelonches <zap407@amerike.edu.mx>
2022-03-16 10:32:47 -0600

    Slow WIP

--- Assets/Scripts/Player/Slow.cs
+++ Assets/Scripts/Player/Slow.cs
@@ -1,16 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class Slow : MonoBehaviour
+
 {
+    [SerializeField] private int _slowcharge = 2;
+    [SerializeField] private bool _flagslow;
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) 
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            Time.timeScale = 0.2f;
+            _flagslow = true;
+            if (_slowcharge > 0 && _flagslow)
+            {
+                StartCoroutine("SlowFunction");
+            }
         }
         else
         {
-            Time.timeScale = 1f;
+            Time.timeScale = 1;
         }
+    }
+    IEnumerator SlowFunction()
+    {
+        print("entre");
+        _flagslow = false;
+        _slowcharge--;
+        Time.timeScale = .2f;
+        yield return new WaitForSeconds(3);
     }
 }
