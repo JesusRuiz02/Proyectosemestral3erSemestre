6a0f5f8c797dccd146e6644ec20a07a12ccaf78f
JesusRuiz02 <zap295@amerike.edu.mx>
2022-03-20 19:43:37 -0600

    fix the dimension of the cameras and add some enemies

--- Assets/Enable_Camera.cs
+++ Assets/Enable_Camera.cs
@@ -1,15 +1,20 @@
-using System;
 using UnityEngine;
 
 public class Enable_Camera : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        GameObject.FindWithTag("MainCamera").GetComponent<FollowCamera>().enabled = true;
+        if (other.CompareTag("Player"))
+        {
+            GameObject.FindWithTag("MainCamera").GetComponent<FollowCamera>().enabled = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        GameObject.FindWithTag("MainCamera").GetComponent<FollowCamera>().enabled = false;
+        if (other.CompareTag("Player"))
+        {
+            GameObject.FindWithTag("MainCamera").GetComponent<FollowCamera>().enabled = false;
+        }
     }
 }
--- Assets/Scripts/Enemy/MagicBullet.cs
+++ Assets/Scripts/Enemy/MagicBullet.cs
@@ -3,33 +3,20 @@ using UnityEngine;
 public class MagicBullet : MonoBehaviour
 {
     [SerializeField] private int _speed = 3;
-    [SerializeField] private float _limitdistance = 13f;
-    
+
     void Update()
     {
         BulletMotion(_speed);
-        EndOfTheBullet(_limitdistance);
     }
 
     private void BulletMotion(int bulletspeed)
     {
-        transform.Translate(Vector3.right * bulletspeed * Time.deltaTime);
-    }
-
-    private void EndOfTheBullet( float limit)
-    {
-        if (transform.position.x >= limit)
-        {
-           Destroy(gameObject); 
-        }
+        transform.Translate(transform.right * bulletspeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }
 
