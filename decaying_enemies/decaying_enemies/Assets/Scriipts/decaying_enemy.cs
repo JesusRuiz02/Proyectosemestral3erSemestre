3d777726a107cb116bd6a6ba969d31f5d0d2cf08
Trinidad Felipe Barajas Gonzalez <85629754+Pollogamerz@users.noreply.github.com>
2022-03-29 08:51:55 -0600

    Update_correction_decayingenemy

--- decaying_enemies/decaying_enemies/Assets/Scriipts/decaying_enemy.cs
+++ decaying_enemies/decaying_enemies/Assets/Scriipts/decaying_enemy.cs
@@ -4,29 +4,29 @@ public class decaying_enemy : MonoBehaviour
     [SerializeField] private float _speed = 30;
     [SerializeField] private float _reversespeed = 20;
     [SerializeField] private float _playerDistance = 5;
-    [SerializeField] private float _visionrange = 7;
-    [SerializeField] private float _reverserange = 7;
-    [SerializeField] private float _firingrange = 5;
-    [SerializeField] private Transform _player;
+    [SerializeField] private float _visionRange = 7;
+    [SerializeField] private float _reverseRange = 7;
+    [SerializeField] private float _firingRange = 5;
+    [SerializeField] private Transform _player = default;
     [SerializeField] private Rigidbody2D rb2D;
 
     private void Update()
     {
         _playerDistance = Vector2.Distance(_player.position, rb2D.position);
-        if (_playerDistance < _visionrange && _playerDistance > _reverserange && _playerDistance > _firingrange)
+        if (_playerDistance < _visionRange && _playerDistance > _reverseRange && _playerDistance > _firingRange)
         {
             Vector2 _target = new Vector2(_player.position.x, _player.position.y);
             Vector2 _newPos = Vector2.MoveTowards(rb2D.position, _target, _speed * Time.deltaTime);
             rb2D.MovePosition(_newPos);
         }
 
-        else if (_playerDistance < _visionrange && _playerDistance > _reverserange && _playerDistance <= _firingrange)
+        else if (_playerDistance < _visionRange && _playerDistance > _reverseRange && _playerDistance <= _firingRange)
         {
             Vector2 _target = new Vector2(_player.position.x, _player.position.y);
             Vector2 _newPos = Vector2.MoveTowards(rb2D.position, _target, 0 * Time.deltaTime);
             rb2D.MovePosition(_newPos);
         }
-        else if (_playerDistance < _reverserange)
+        else if (_playerDistance < _reverseRange)
         {
             Vector2 _target = new Vector2(_player.position.x, _player.position.y);
             Vector2 _newPos = Vector2.MoveTowards(rb2D.position, _target, _reversespeed * Time.deltaTime);
