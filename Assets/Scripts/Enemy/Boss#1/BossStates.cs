2b0d96f45fe9f5701bde07047df85aa847adf73a
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2022-05-10 08:40:04 -0500

    done spore attacks

--- Assets/Scripts/Enemy/Boss#1/BossStates.cs
+++ Assets/Scripts/Enemy/Boss#1/BossStates.cs
@@ -9,13 +9,16 @@ public class BossStates : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody2D = default;
     [SerializeField] private float _randomState = default;
-    [SerializeField] private float _smashtimer = default;
+    [SerializeField] private float _power = 1200;
+    [SerializeField] private bool _facingRight = true;
     [Header("GameObjects")]
     [SerializeField] private GameObject _whipCollider = default;
     [SerializeField] private GameObject _targetPlayer = default;
     [SerializeField] private GameObject _smashColliderRight = default;
     [SerializeField] private GameObject _smashColliderLeft = default;
     [SerializeField] private GameObject _particle = default;
+    [SerializeField] private GameObject _spores = default;
+    
     
     [Header("Vectors")]
     [SerializeField] private Vector3[] _pathvalue = new Vector3[3];
@@ -25,24 +28,30 @@ public class BossStates : MonoBehaviour
     [SerializeField] private Vector2 _smashpoint = Vector2.zero;
     [SerializeField] private Vector2 _targetCorner1 = default; 
     [SerializeField] private Vector2 _targetCorner2 = default;
+    [SerializeField] private Transform[] firepoints;
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         RandomStatePicker();
-        
     }
 
     private void RandomStatePicker()
     {
-        _randomState = Random.Range(0, 1);
+        _randomState = Random.Range(0, 3);
         if (_randomState == 1)
         {
-            StartCoroutine(WhipAttack());
+            StartCoroutine(ThrowingSpore());
         }
         else if (_randomState == 0)
         {
-          Smash();
+            Smash();
         }
+        else if (_randomState == 2)
+        {
+            AttackPlayer();
+        }
+        
     }
 
     private IEnumerator WhipAttack()
@@ -87,16 +96,40 @@ public class BossStates : MonoBehaviour
         InvokeRepeating("CreateSmashCollider", 3.1f, 400f);
     }
 
-    private void ThrowingSpores()
+    private IEnumerator ThrowingSpore()
     {
         _targetLocation = transform.position;
         var sequence = DOTween.Sequence();
         sequence.Append(transform.DOMove(_smashpoint, 1f)).SetEase(Ease.Linear);
-        
+        yield return new WaitForSeconds(2f);
+        for (int i = 0; i < 3; i++)
+        {
+            var newSpore = Instantiate(_spores,firepoints[i].position , firepoints[i].rotation);
+            newSpore.GetComponent<Rigidbody2D>().AddForce(firepoints[i].right *  _power, ForceMode2D.Impulse);
+        }
+        RandomStatePicker();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        
+        FlippingSprite();
+    }
+
+    private void FlippingSprite()
+    {
+        if (transform.position.x > _targetPlayer.transform.position.x && !_facingRight)
+        {
+           Flip(); 
+        }
+        else if (transform.position.x < _targetPlayer.transform.position.x && _facingRight)
+        {
+            Flip();
+        }
+    }
+    
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
