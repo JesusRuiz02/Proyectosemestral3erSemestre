580ea2d790a9f60ea382553dfb8bbfd07e2b0fe1
Justpeli <83651771+Justpeli@users.noreply.github.com>
2022-05-18 09:24:20 -0500

    ignora esto chuy

--- Assets/Scripts/Player/Slow.cs
+++ Assets/Scripts/Player/Slow.cs
@@ -4,29 +4,16 @@ using UnityEngine;
 
 public class Slow : MonoBehaviour
 {
-    [SerializeField] private bool isSlow;
-    [SerializeField] private int _slowCharges=2;
-
+ 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.W) && !isSlow && _slowCharges>0)
+        if (Input.GetKey(KeyCode.W) )
         {
-            StartCoroutine("SlowFunction");
+            Time.timeScale = .2f;
         }
-        else if (!isSlow)
+        else 
         {
             Time.timeScale = 1;
-        }
-    }
-    IEnumerator SlowFunction()
-    {
-        if (_slowCharges>0)
-        {
-            isSlow = true;
-            Time.timeScale = .2f;
-            yield return new WaitForSeconds(.5f);
-            isSlow = false;
-            _slowCharges--;
         }
     }
 }
