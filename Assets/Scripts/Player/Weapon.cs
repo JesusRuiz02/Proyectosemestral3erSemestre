d5d4e499b1f5f5e69dd9b08f212278f4be901fc7
JesusRuiz02 <zap295@amerike.edu.mx>
2022-05-30 15:02:15 -0500

    si

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -4,9 +4,19 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private Transform _firepoint = default;
     [SerializeField] private GameObject _playerbullet = default;
+    [SerializeField] private Vector3 _mousePos  = default;
+    [SerializeField] private Camera _mainCamera = default;
 
+    private void Start()
+    {
+        _mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
     void Update()
     {
+        _mousePos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = _mousePos - transform.position;
+        float RotationZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+       transform.rotation = Quaternion.Euler(0,0,RotationZ);
         if (Input.GetMouseButtonDown(1))
         {
             shoot();
