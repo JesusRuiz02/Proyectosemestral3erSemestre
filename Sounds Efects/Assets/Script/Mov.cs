57143045834c633dd6549e0e4b4b5a23ff4761f3
Trinidad Felipe Barajas Gonzalez <85629754+Pollogamerz@users.noreply.github.com>
2022-05-30 08:27:37 -0500

    Sound_Efects

---
+++ Sounds Efects/Assets/Script/Mov.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Mov : MonoBehaviour
+{
+    [SerializeField] private float velocity = 5f;
+    [SerializeField] private float velocity2 = 7f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * velocity * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * velocity * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
+        {
+            transform.position += Vector3.forward * velocity * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
+        {
+            transform.position += Vector3.back * velocity * Time.deltaTime;
+        }
+    }
+}
